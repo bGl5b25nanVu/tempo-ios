c182a2ada338428061f2c914a916b2b540a64563
liyongjun <liyongjun@chessnet.cn>
2026-04-24 09:28:44 +0800

    feat: storekit2 TempoApp - start TransactionListener on launch

--- App/TempoApp.swift
+++ App/TempoApp.swift
@@ -2,6 +2,11 @@ import SwiftUI
 
 @main
 struct TempoApp: App {
+    init() {
+        // Start listening for App Store transaction updates (purchases, renewals, refunds)
+        TransactionListener.shared.startListening()
+    }
+
     var body: some Scene {
         WindowGroup {
             CalendarView()
